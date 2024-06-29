3acbacc49170ebd9c4de021306000acaa1528f58
VishalSaravanan-tech <vishal.programming@gmail.com>
2024-06-29 23:33:25 +0530

    player component implemented

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -31,7 +31,7 @@ const Sidebar = () => {
                 <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Create Playlist</button>
             </div>
             <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4'>
-                <h1>Lets find some podcasts to follw</h1>
+                <h1>Lets find some podcasts to follow</h1>
                 <p className='font-light'>we'll keep you updated in new episodes</p>
                 <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Browse podcasts</button>
             </div>
